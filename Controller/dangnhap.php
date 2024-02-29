f999739d9c7139d210126920136935e1f476cc80
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-02-29 20:51:59 +0700

    Update dangnhap.php

--- Controller/dangnhap.php
+++ Controller/dangnhap.php
@@ -3,18 +3,21 @@ $act = "dangnhap";
 if (isset($_GET["act"])) {
     $act = $_GET['act'];
 }
+
 switch ($act) {
     case 'dangnhap':
         include_once "./View/login.php";
         break;
 
-    case 'dangnhap_actional':
+    case 'dangnhap_action':
+        echo $act;
         # gửi thông tin đăng nhập về lên
         $user = '';
         $pass = '';
         if (isset($_POST['submit'])) {
             $user = $_POST['txtusername'];
             $pass = $_POST['txtpassword'];
+            echo $user;
             $saltF = "G45a#?";
             $saltL = "Td23$%";
             // $passnew=md5($saltF.$pass.$salfL);
@@ -23,7 +26,7 @@ switch ($act) {
             // controller yêu cầu hiển thị thông tin có hay không? model
             $kh = new user();
             $lgkh = $kh->loginKH($user, $passnew);
-            if ($lgkh !== false) {
+            if ($lgkh !== null) {
                 echo '<script> alert("Đăng nhập thành công"); </script>';
                 // nếu như đăng nhập thành công thì lưu thông tin vào trong session;
                 // $khang=$kh->loginKH($user, $passnew);
@@ -41,4 +44,5 @@ switch ($act) {
         unset($_SESSION['tenkh']);
         echo '<script> alert("Bạn có muốn đăng xuất ?");</script>';
         echo '<meta http-equiv="refresh" content="0";url=../index.php?action=home"/>';
+        break;
 }
