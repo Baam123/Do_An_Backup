6f03e5b8589027675d9924d3ebdc6caeef44852f
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-02-29 22:07:17 +0700

    Update dangnhap.php

--- Controller/dangnhap.php
+++ Controller/dangnhap.php
@@ -10,32 +10,29 @@ switch ($act) {
         break;
 
     case 'dangnhap_action':
-        echo $act;
         # gửi thông tin đăng nhập về lên
         $user = '';
         $pass = '';
         if (isset($_POST['submit'])) {
             $user = $_POST['txtusername'];
             $pass = $_POST['txtpassword'];
-            echo $user;
             $saltF = "G45a#?";
             $saltL = "Td23$%";
             // $passnew=md5($saltF.$pass.$salfL);
             $passnew = md5($pass);
-            echo $passnew;
             // controller yêu cầu hiển thị thông tin có hay không? model
             $kh = new user();
             $lgkh = $kh->loginKH($user, $passnew);
-            if ($lgkh !== null) {
+            if ($lgkh !== false) {
                 echo '<script> alert("Đăng nhập thành công"); </script>';
                 // nếu như đăng nhập thành công thì lưu thông tin vào trong session;
                 // $khang=$kh->loginKH($user, $passnew);
                 $_SESSION['makh'] = $lgkh['makh'];
                 $_SESSION['tenkh'] = $lgkh['tenkh'];
-                echo '<meta http-equiv="refresh" content="0;url=../index.php?action=home"/>';
+                echo '<meta http-equiv="refresh" content="0;url=./index.php?action=home"/>';
             } else {
                 echo '<script> alert("Đăng nhập không thành công");</script>';
-                echo '<meta http-equiv="refresh" content="0;url=../index.php?action=dangnhap"/>';
+                echo '<meta http-equiv="refresh" content="0;url=./index.php?action=dangnhap"/>';
             }
         }
         break;
